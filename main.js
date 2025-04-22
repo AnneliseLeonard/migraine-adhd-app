221df9c8015fec91872a1b40c222ea779a101b00
AnneliseLeonard <annelise.leonard2@gmail.com>
2025-04-21 19:13:24 -0500

    navbar base

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
 
 const app = createApp({
     
-})
+}).mount('#app')
